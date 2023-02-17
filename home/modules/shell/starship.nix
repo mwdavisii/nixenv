aaac96b08295bf9bef4f0856682c5eab2fd9b321
Mike D <mwdavisii@gmail.com>
2023-02-17 14:56:26 -0600

    Updated Starship

--- home/modules/shell/starship.nix
+++ home/modules/shell/starship.nix
@@ -14,8 +14,73 @@ in
       enableBashIntegration = true;
       enableZshIntegration = true;
       package = pkgs.starship;
-    };
+      settings = {
+        scan_timeout = 10;
+        add_newline = false;
+        package = {
+          disabled = true;
+        };
+        aws = {
+          format = "on [$symbol($profile )(\($region\) )]($style)";
+          style = "bold blue";
+          symbol = "🅰 ";
+          region_aliases = {
+            ap-northeast-1 = "jp";
+            us-west-2 = "Oregon";
+          };
+        };
+        conda = {
+          format = "[$symbol$environment](dimmed green) ";
+        };
+        nodejs = {
+          format = "via [🤖 $version](bold green) ";
+        };
+        #git_branch = {
+        #    symbol = "🌱 ";
+        #    truncation_length = 4;
+        #    truncation_symbol = "";
+        #};
+        git_commit = {
+          commit_hash_length = 4;
+          tag_symbol = "🔖 ";
+        };
+        git_status = {
+            conflicted = "🏳";
+            ahead = "⇡$count";
+            diverged = "⇕⇡$ahead_count⇣$behind_count";
+            behind = "⇣$count";
+            untracked = "🤷";
+            stashed = "📦";
+            modified = "📝";
+            staged = "[++\($count\)](green)";
+            renamed = "👅";
+            deleted = "🗑";
+        };
+        golang = {
+          format = "via [🏎💨 $version](bold cyan) ";
+        };
+        kubernetes = {
+          format = "on [⛵ ($user on )($cluster in )$context \($namespace\)](dimmed green) ";
+          disabled = false;
+          detect_files = ["gitops" "eks"];
+          context_aliases = {
+            "dev.local.cluster.k8s" = "dev";
+          };
+        };
 
-    xdg.configFile."starship".source = ../../../config/.config/starship;
+        opa = {
+          format = "via [⛑️  $version](bold red) ";
+        };
+        python = {
+          symbol = "👾 ";
+          pyenv_version_name = true;
+        };
+        time = {
+          disabled = false;
+          time_format = "%R"; # Hour:Minute Format
+          format = "🕙 $time ";
+        };
+      };
+    };
   };
 }
